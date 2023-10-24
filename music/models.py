2e2f280f8fa9a8deb0f8ac0a5b0db2c5859958fe
Zachary Higgs <thespecialistlives@gmail.com>
2023-10-24 12:00:15 +1000

    Renamed Events to Event

--- music/models.py
+++ music/models.py
@@ -16,7 +16,7 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"Name: {self.name}"
 
-class Events(db.Model):
+class Event(db.Model):
     __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
