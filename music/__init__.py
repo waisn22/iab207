d2c6e3f3996093b3c0da18b2d43954d4b1f2ca72
taylo <taylor.smith.11111@gmail.com>
2023-10-30 18:38:38 +1000

    import render_template

--- music/__init__.py
+++ music/__init__.py
@@ -1,5 +1,5 @@
 #from package import Class
-from flask import Flask 
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
