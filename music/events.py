9b768830cdda69bc45b6e9ad527ecd3f92653e69
Zachary Higgs <thespecialistlives@gmail.com>
2023-11-03 05:19:41 +1000

    Removing useless comments

--- music/events.py
+++ music/events.py
@@ -10,12 +10,6 @@ from flask_login import login_required, current_user
 eventbp = Blueprint('event', __name__, url_prefix='/events')
 
 
-#@eventbp.route('/<id>')
-#def show(id):
- #   event = db.session.scalar(db.select(Event).where(Event.id==id))
-    # create the comment form
- #   form = CommentForm()    
-  #  return render_template('event/show.html', event=event, form=form)
 
 @eventbp.route('/show', methods=['GET', 'POST'])
 def show():
